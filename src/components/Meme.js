fb2fe1085a2013c3ed7e793369ab4d5517e59583
Shankar989 <96803519+Shankar989@users.noreply.github.com>
2022-11-15 12:46:46 +0530

    Add files via upload

---
+++ src/components/Meme.js
@@ -0,0 +1,34 @@
+import memesData from "../memeData"
+import React from 'react';
+export default function Meme(){
+
+    const [memeImage,setMemeImage] = React.useState("")
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        console.log(memesArray[randomNumber].url)
+    }
+    return(
+        <main>
+            <div className="form">
+                <input 
+                type="text" 
+                placeholder ="Top text" 
+                className="form-input" 
+                />
+                <input 
+                type="text" 
+                placeholder="Bottom text" 
+                className="form-input"
+                />
+                <button 
+                className="form-button"
+                onClick={getMemeImage}
+                >Get a new meme image 🖼️
+                 </button>
+            </div>
+            <img src={memeImage} className="meme--image"/>
+        </main>
+    )
+}
